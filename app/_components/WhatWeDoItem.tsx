c1631595d4875447ffdab6824d6957388a551641
Milad Joodi <winbeta.com@gmail.com>
2024-04-08 23:01:03 +0330

    up

--- app/_components/WhatWeDoItem.tsx
+++ app/_components/WhatWeDoItem.tsx
@@ -9,7 +9,7 @@ type WeDoTypes = {
 
 const WhatWeDoItem = ({item}:{item: WeDoTypes}) => {
   return (
-    <div className="flex flex-col justify-center items-center w-[90%]  gap-4 p-8 rounded-3xl shadow-lg hover:shadow-2xl transition-all duration-300">
+    <div className="flex flex-col justify-center items-center w-[90%]  gap-4 p-8 rounded-3xl shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer">
       <div className={`shadow-lg rounded-lg p-2 w-[46px] h-[46px] ` +item?.color}>
         <Image src={item.image} width={30} height={30} alt="icon" className=" " />
       </div>
