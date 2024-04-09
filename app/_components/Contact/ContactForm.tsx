d93b7ec59a9abf30941b0e24a4e0cfdac94b5f0a
Milad Joodi <winbeta.com@gmail.com>
2024-04-09 16:00:41 +0330

    up

--- app/_components/Contact/ContactForm.tsx
+++ app/_components/Contact/ContactForm.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const ContactForm = () => {
   return (
     <div className="container mx-auto py-3 mt-12 sm:py-4">
-      <div className="p-8 border  flex gap-6 justify-between ">
+      <div className="xl:p-8 rounded-2xl flex flex-col lg:flex-row gap-6 justify-between ">
         {/* left */}
-        <div className=" shadow-2xl bg-white rounded-2xl p-24 w-full flex flex-col gap-4">
+        <div className=" shadow-2xl bg-white  lg:p-24 p-4 pb-10 w-full flex flex-col gap-6">
                 <h4 className="text-xl font-semibold text-gray-500">Send Message</h4>
 
                 <div className="flex flex-col gap-4">
-                    <div className="flex gap-8 w-full">
-                        <input type="text" className="flex-1 p-6 bg-gray-200 rounded-md text-lg outline-none" placeholder="Your Name"/>
-                        <input type="text" className="flex-1 shrink p-6 bg-gray-200 rounded-md text-lg outline-none" placeholder="Your Email"/>
+                    <div className="flex flex-col lg:flex-row lg:gap-8 gap-4 w-full">
+                        <input type="text" className="flex-1 lg:p-6 p-4 bg-gray-200 rounded-md text-lg outline-none" placeholder="Your Name"/>
+                        <input type="text" className="flex-1 lg:p-6 p-4  bg-gray-200 rounded-md text-lg outline-none" placeholder="Your Email"/>
                     </div>
                     <textarea className="p-4 bg-gray-200 rounded-md text-lg outline-none" name="" id="" cols={30} rows={10}></textarea>
                 </div>
@@ -30,28 +30,56 @@ const ContactForm = () => {
         </div>
 
         {/* right */}
-        <div className=" shadow-2xl bg-green-100 xl:py-16 px-8">
-        <h4 className="text-xl font-semibold">Send Message</h4>
-        <ul>
-            <li>
+        <div className="text-gray-500 flex flex-col lg:p-24 p-4 pb-10  gap-6 shadow-2xl bg-white rounded-2xl xl:py-24 px-16">
+        <h4 className="text-xl font-semibold ">Send Message</h4>
+        <ul className="flex flex-col gap-6">
+            <li className="flex gap-2 items-center">
+            <Image
+                    src="/contact/email.svg"
+                    width={24}
+                    height={24}
+                    alt=""
+                    />
                 hello@mail.com
             </li>
-            <li>
-                hello@mail.com
+            <li className="flex gap-2 items-center">
+            <Image
+                    src="/contact/world.svg"
+                    width={24}
+                    height={24}
+                    alt=""
+                    />
+                www.abc.com
             </li>
-            <li>
+            <li className="flex gap-2 items-center">
+            <Image
+                    src="/contact/location.svg"
+                    width={24}
+                    height={24}
+                    alt=""
+                    />
             Sudirman street, holgan, melbourne
             </li>
-            <li>
+            <li className="flex gap-2 items-center">
+            <Image
+                    src="/contact/tel.svg"
+                    width={24}
+                    height={24}
+                    alt=""
+                    />
             1234 - 5678
             </li>
         </ul>
 
-        <h4 className="text-xl font-semibold">Send Message</h4>
-
+        <h4 className="text-xl font-semibold ">Social Media</h4>
 {/* social */}
 <div className="flex gap-6">
-instagram
+<Image
+                    src="/contact/social.png"
+                    width={250}
+                    height={55}
+                    alt=""
+                    />
 </div>
 
         </div>
